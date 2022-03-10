45942452c31e863f98b9d5294418e6d224685d3f
Nduduzo <nduduzok@pbsa.co.za>
2022-03-10 20:38:30 +0200

    LATEST

---
+++ Configuration/JwtConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace WebAPI_with_Blazor.Configuration
+{
+    public class JwtConfig
+    {
+        public string Secret { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,46 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TodoApp.Data;
+using WebAPI_with_Blazor.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+//Read Config and Mapp it 
+builder.Services.Configure<JwtConfig>(builder.Configuration.GetSection("JwtConfig"));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//
+builder.Services.AddDbContext<ApiDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+//auth
+builder.Services.AddAuthentication(op =>
+{
+    op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(jwt =>
+    {
+        var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtConfig:Secret"]);
+        jwt.SaveToken = true;
+        jwt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            RequireExpirationTime = false
+        };
+    });
+
+//
+//builder.Services.AddDefaultIdentity<IdentityUser>();
 
 var app = builder.Build();
 
@@ -19,6 +54,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
+
 
 app.MapControllers();
 
